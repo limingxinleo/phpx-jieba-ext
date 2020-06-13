f3f31118e2254bd17e1b6beacaf327a08588d262
李铭昕 <715557344@qq.com>
2020-06-13 14:43:45 +0800

    项目初始化

---
+++ jieba/src/main.cpp
@@ -0,0 +1,48 @@
+#include "phpx.h"
+#include "cppjieba/Jieba.hpp"
+#include <iostream>
+
+using namespace php;
+using namespace std;
+
+const char* const DICT_PATH = "../../dict/jieba.dict.utf8";
+const char* const HMM_PATH = "../../dict/hmm_model.utf8";
+const char* const USER_DICT_PATH = "../../dict/user.dict.utf8";
+const char* const IDF_PATH = "../../dict/idf.utf8";
+const char* const STOP_WORD_PATH = "../../dict/stop_words.utf8";
+
+PHPX_FUNCTION(jieba_hello)
+{
+    echo("argc=%d\n", args.count());
+    retval = DICT_PATH;
+}
+
+PHPX_EXTENSION()
+{
+    Extension *extension = new Extension("jieba", "0.0.1");
+
+    extension->onStart = [extension]() noexcept {
+        extension->registerConstant("JIEBA_VERSION", 10001);
+    };
+
+    //extension->onShutdown = [extension]() noexcept {
+    //};
+
+    //extension->onBeforeRequest = [extension]() noexcept {
+    //    cout << extension->name << "beforeRequest" << endl;
+    //};
+
+    //extension->onAfterRequest = [extension]() noexcept {
+    //    cout << extension->name << "afterRequest" << endl;
+    //};
+
+    extension->info({"jieba support", "enabled"},
+                    {
+                        {"version", extension->version},
+                        {"author", "limingxinleo <l@hyperf.io>"},
+                        {"date", "2020-06-13"},
+                    });
+    extension->registerFunction(PHPX_FN(jieba_hello));
+
+    return extension;
+}

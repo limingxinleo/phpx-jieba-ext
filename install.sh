bf9b6a587704004153c3c5bcd75628c5193e2e32
李铭昕 <715557344@qq.com>
2020-06-13 21:14:44 +0800

    Update

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 cp -r dict /dict
 DIR=$(php-config --extension-dir)
-cp -r jieba/lib/jieba.so $DIR
+cp -r jieba/lib/jieba-$1.so $DIR/jieba.so
 CONFD=$(php -i | grep "Scan this dir for additional .ini files" | awk '{print $9}')
 cp -r 51_jieba.ini $CONFD
